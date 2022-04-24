28d6c102db9c452a0a90d0412c13ce47c0882532
amithreddy258 <20bds006@iiitdwd.ac.in>
2022-04-24 19:27:52 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/patient_record/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.patient_record
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: ,
+    );
+  }
+}
